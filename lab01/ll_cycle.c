6af9d50eba5e31bc6401574f4b46a049403a41ac
Methyl Orange <methyl_orange@126.com>
2025-04-10 22:40:55 +0800

    finish lab01

--- lab01/ll_cycle.c
+++ lab01/ll_cycle.c
@@ -2,6 +2,15 @@
 #include "ll_cycle.h"
 
 int ll_has_cycle(node *head) {
-    /* your code here */
+    node* fast = head;
+    node* slow = head;
+    while(fast != NULL) {
+        fast = fast->next;
+        if(fast == NULL) break;
+        fast = fast->next;
+        if(fast == NULL) break;
+        slow = slow->next;
+        if(fast == slow) return 1;
+    }
     return 0;
 }
